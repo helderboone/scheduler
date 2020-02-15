117bf4a0e0736f4f07251e43c73bf79d089d55af
Helder Boone <helder.boone@gmail.com>
2020-02-15 01:22:35 -0300

    Set default route

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
   {
     path: 'appointments',
     component: AppointmentGetComponent
+  },
+  { path: '',
+    redirectTo: '/appointments',
+    pathMatch: 'full'
   }
 ];
 
