b5c6e9742debcfc6103124ab2a3a9f1adfd5dabd
Helder Boone <helder.boone@gmail.com>
2020-02-17 17:34:28 -0300

    Create delete with success and create with error tests

--- Test/AppointmentTests.cs
+++ Test/AppointmentTests.cs
@@ -1,4 +1,5 @@
 using API;
+using API.Error;
 using API.Features.Appointment;
 using API.Infra;
 using AutoMapper;
@@ -7,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -38,14 +40,14 @@ namespace Test
             _mediator = services.BuildServiceProvider().GetService<IMediator>();
         }
 
-        
+
         [Fact]
         public async Task Should_Create_With_Success()
         {
             var command = new Create.Command
             {
                 Id = 1,
-                PatientName = "Joao Teste",
+                PatientName = "Joao",
                 PatientBirthdate = DateTime.Now.AddYears(-30),
                 StartDate = new DateTime(2020, 03, 01, 09, 00, 00),
                 EndDate = new DateTime(2020, 03, 01, 10, 00, 00),
@@ -55,6 +57,36 @@ namespace Test
             var actual = await _mediator.Send(command);
 
             Assert.Equal(Unit.Value, actual);
+        }
+
+        [Fact]
+        public async Task Should_Not_Create_And_Return_BadRequest400()
+        {
+            var command = new Create.Command
+            {
+                Id = 1,
+                PatientName = "Joao",
+                PatientBirthdate = DateTime.Now.AddYears(-30),
+                StartDate = new DateTime(2020, 03, 01, 09, 00, 00),
+                EndDate = new DateTime(2020, 03, 01, 10, 00, 00),
+                Observations = string.Empty,
+            };
+
+            await _mediator.Send(command);
+
+            var commandTwo = new Create.Command
+            {
+                Id = 2,
+                PatientName = "Joao Teste",
+                PatientBirthdate = DateTime.Now.AddYears(-20),
+                StartDate = new DateTime(2020, 03, 01, 09, 00, 00),
+                EndDate = new DateTime(2020, 03, 01, 10, 00, 00),
+                Observations = string.Empty,
+            };
+
+            var ex = await Assert.ThrowsAsync<RestException>(() => _mediator.Send(commandTwo));
+
+            Assert.Equal(HttpStatusCode.BadRequest, ex.Code);
         }
 
         [Fact]
@@ -68,5 +100,27 @@ namespace Test
 
             Assert.Equal(expected, actual);
         }
-    }    
+
+        [Fact]
+        public async Task Should_Delete_With_Success()
+        {
+            var command = new Create.Command
+            {
+                Id = 1,
+                PatientName = "Joao",
+                PatientBirthdate = DateTime.Now.AddYears(-30),
+                StartDate = new DateTime(2020, 03, 01, 09, 00, 00),
+                EndDate = new DateTime(2020, 03, 01, 10, 00, 00),
+                Observations = string.Empty,
+            };
+
+            await _mediator.Send(command);
+
+            var commandDelete = new Delete.Command { Id = 1 };
+
+            var actual = await _mediator.Send(commandDelete);
+
+            Assert.Equal(Unit.Value, actual);
+        }
+    }
 }
